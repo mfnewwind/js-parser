a1b3c4b9b0b73fd91e1c3ee5b3b5fcc742085fc9
Pine Mizune <pinemz@gmail.com>
2015-04-21 10:36:59 +0900

    parseFile のテストを追加

---
+++ test/files/input.js
@@ -0,0 +1,10 @@
+/*globals console */
+
+(function () {
+  'use strict';
+  
+  var N = 10;
+  for (var i = 0; i < N; ++i) {
+    console.log('n = ' + i);
+  }
+})();
--- test/parser-test.js
+++ test/parser-test.js
@@ -1,19 +1,57 @@
 'use strict';
 
 /*jslint node: true */
-/*globals describe, it */
+/*jslint -W030 */
+/*globals describe, it, beforeEach, afterEach */
+
+
+var Path = require('path');
 
 var _ = require('lodash');
 var expect = require('chai').expect;
+var sinon = require('sinon');
 
 var JsParser = require('../lib/parser');
 
+
+var INPUT_PATH = Path.resolve(__dirname, 'files/input.js');
+
+
 describe('Unit test for lib/parser.js', function () {
+  var sandbox;
+  
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+  });
+  
+  afterEach(function () {
+    sandbox.restore();
+  });
+  
   describe('#constructor', function () {
     it('should create JsParser instance without errors', function () {
       expect(function () {
         new JsParser();
       }).to.not.throws();
+    });
+  });
+  
+  describe('#parseFile', function () {
+    it('should not throw to parse source file', function (done) {
+      var parser = new JsParser();
+      var parse = sandbox.stub(parser, "parse", function (source, options, cb) {
+        expect(source).to.be.a('string');
+        expect(options).to.be.an('object');
+        expect(cb).to.be.a('function');
+        
+        cb(null, []);
+      });
+      
+      parser.parseFile(INPUT_PATH, function (err, result) {
+        expect(parse).to.have.been.calledOnce;
+        expect(result).to.be.an('array');
+        done(err);
+      });
     });
   });
 });
