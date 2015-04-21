23c36b37da522c9aca06349d062a3f2b2b893b3d
Pine Mizune <pinemz@gmail.com>
2015-04-21 13:38:57 +0900

    parseVariables のテストを追加

--- test/variable-test.js
+++ test/variable-test.js
@@ -11,19 +11,35 @@ var _ = require('lodash');
 var expect = require('chai').expect;
 var sinon = require('sinon');
 
+
+var JsParser = require('../lib/parser');
 var parseVariables = require('../lib/variable');
 
 
 describe('Unit test for lib/variable.js', function () {
-  var sandbox;
+  var sandbox, parser;
   
   beforeEach(function () {
     sandbox = sinon.sandbox.create();
+    parser = new JsParser();
   });
   
   afterEach(function () {
     sandbox.restore();
   });
   
-  it('parseVariables');
+  describe('parseVariables', function () {
+    it('should parse simple source', function () {
+      var ast = parser.toAST('var name = "value";');
+      var result = parseVariables(ast);
+      
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.have.property('type').that.is.string('variable');
+      expect(result[0]).to.have.property('name').that.is.string('name');
+      expect(result[0]).to.have.property('class_name').that.is.string('');
+      expect(result[0]).to.have.property('comment').that.is.a('string');
+      expect(result[0]).to.have.property('line').that.is.a('number');
+    });
+  });
 });
